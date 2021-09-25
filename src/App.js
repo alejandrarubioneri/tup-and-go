a01175b2e2a3ab6cfe1cc3b49c0852380e69f99e
Alejandra Rubio <alejandrarubio@MacBook-Air-de-Alberto-2.local>
2021-09-25 14:19:29 +0200

    project started

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <h1>Tap&Go!</h1>
+      <p>Find and share easy tupper recipes inspiration with Tup&Go!</p>
+      
+    </div>
+  );
+}
+
+export default App;
